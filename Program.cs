a31af82548d2d130f7e016c647fa63749e77994f
WiktoriaWezgraj <wiktoriawezgraj1@gmail.com>
2024-10-16 13:20:12 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string secretWord = "giraffe";
+        string guess = "";
+        int guessCount = 0;
+        int guessLimit = 3;
+        bool outofGuesses = false;
+
+        while (guess != secretWord && !outofGuesses)
+        {
+            if (guessCount < guessLimit)
+            {
+                Console.Write("Enter guess: ");
+                guess = Console.ReadLine();
+                guessCount++;
+            }
+            else
+            {
+                outofGuesses = true;
+            }
+        }
+        if (outofGuesses)
+        {
+            Console.Write("You lose!");
+        }
+        else
+        {
+            Console.Write("You win!");
+
+        }
+
+        Console.ReadLine();
+    }
+}
